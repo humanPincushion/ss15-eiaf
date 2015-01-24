8ef66121085efc219fa390dd65fdbccfc1759b11
Matt Scheurich <matt@lvl99.com>
2015-01-25 01:21:46 +1100

    disable auto play of tracks when viewing playlist

--- src/js/app/_com.app.dir.track.js
+++ src/js/app/_com.app.dir.track.js
@@ -4,7 +4,7 @@
  * @ngdoc directive
  * @name app.directive.mxTrack
  * @function
- * 
+ *
  * @description
  * Populates media meta data into a track within the playlist.
  *
@@ -18,27 +18,27 @@ app.directive('mxTrack', ['mediaSvc', 'playerSvc', function(mediaSvc, playerSvc)
   return {
     restrict: 'EA',
     templateUrl: '/ng/track.tpl.html',
-    link: function($scope, $element) { 
-      
+    link: function($scope, $element) {
+
       var allowedDomains = ['soundcloud.com'],
           search = $scope.track.urls[0].match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i),
           domain = search && search[1];
-      
+
       //console.log( $.inArray( domain, allowedDomains ) );
-      
-      if( $.inArray( domain, allowedDomains ) !== -1 ) { 
+
+      if( $.inArray( domain, allowedDomains ) !== -1 ) {
         //console.log( $scope.track.urls[0] );
-        mediaSvc.getMeta( $scope.track.urls[0] ).then(function(media) { 
+        mediaSvc.getMeta( $scope.track.urls[0] ).then(function(media) {
           $scope.media = media;
           //console.log('media');
-          playerSvc.playTrack( $scope.track.urls[0] );
-          
+          //playerSvc.playTrack( $scope.track.urls[0] );
+
         });
       } else {
         $scope.media = false;
       }
-      
-      
+
+
     }
   };
 }]);
