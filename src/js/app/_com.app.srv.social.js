469898adcaea1c49e8bb106ba9875807dbbdc62b
humanPincushion <viren@viren.com.au>
2015-01-26 01:18:58 +1100

    Linkify twitter usernames

--- src/js/app/_com.app.srv.social.js
+++ src/js/app/_com.app.srv.social.js
@@ -25,22 +25,26 @@ app.factory('socialSvc', ['$http', '$q', function($http, $q) {
   
   function linkify(str){
     // order matters
-    var re = [
+    var replace = [
         "\\b((?:https?|ftp)://[^\\s\"'<>]+)\\b",
         "\\b(www\\.[^\\s\"'<>]+)\\b",
         "\\b(\\w[\\w.+-]*@[\\w.-]+\\.[a-z]{2,6})\\b", 
-        "#([a-z0-9]+)"];
-    re = new RegExp(re.join('|'), 'gi');
+        "#([a-z0-9_]+)",
+        "@([a-z0-9_]+)"
+    ];
+    replace = new RegExp(replace.join('|'), 'gi');
 
-    return str.replace(re, function(match, url, www, mail, twitter){
+    return str.replace(replace, function(match, url, www, mail, hashtag, twitter){
         if(url)
           return '<a href="' + url + '" target="_blank">' + url + '</a>';
         if(www)
           return '<a href="http://' + www + '" target="_blank">' + www + '</a>';
         if(mail)
           return '<a href="mailto:' + mail + '">' + mail + '</a>';
+        if(hashtag)
+          return '<a href="/play/' + hashtag + '" class="playlist" playlist="#' + hashtag + '">#' + hashtag + '</a>';
         if(twitter)
-          return '<a href="#" class="playlist" playlist="#' + twitter + '">#' + twitter + '</a>';
+          return '<a href="/play/@' + twitter + '" class="playlist" playlist="@' + twitter + '">@' + twitter + '</a>';
         
         return match;
     });
